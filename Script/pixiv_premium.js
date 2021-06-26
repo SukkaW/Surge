680cc6eecc42e8dfa1bee27fe9fdf2821a7b25f1
SukkaW <isukkaw@gmail.com>
2021-06-26 09:12:20 +0800

    Update rules

--- Script/pixiv_premium.js
+++ Script/pixiv_premium.js
@@ -1,6 +1,8 @@
 let body = $response.body;
 body = JSON.parse(body);
-body['response']['user']['is_premium'] = true;
+if (body?.response?.user?.is_premium) {
+  body.response.user.is_premium = true;
+}
 body = JSON.stringify(body);
 
 $done({ body })
