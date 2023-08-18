6b324f4079eed89440e9f936d57f96f14524ffd5
SukkaW <isukkaw@gmail.com>
2023-08-18 23:04:14 +0800

    Build domestic conf

---
+++ Build/build-domestic-ruleset.js
@@ -0,0 +1,58 @@
+// @ts-check
+const path = require('path');
+const { DOMESTICS } = require('../Source/non_ip/domestic');
+const { readFileByLine } = require('./lib/fetch-remote-text-by-line');
+const { processLine } = require('./lib/process-line');
+const { withBannerArray } = require('./lib/with-banner');
+const { compareAndWriteFile } = require('./lib/string-array-compare');
+
+(async () => {
+  const rl = readFileByLine(path.resolve(__dirname, '../Source/non_ip/domestic.conf'));
+  const results = [];
+  for await (const l of rl) {
+    const line = processLine(l);
+    if (line) {
+      results.push(line);
+    }
+  }
+
+  results.push(
+    ...Object.entries(DOMESTICS)
+      .filter(([key]) => key !== 'SYSTEM')
+      .flatMap(([, { domains }]) => domains)
+      .sort()
+      .map((domain) => `DOMAIN-SUFFIX,${domain}`)
+  );
+
+  await Promise.all([
+    compareAndWriteFile(
+      withBannerArray(
+        'Sukka\'s Surge Rules - Domestic Domain',
+        [
+          'License: AGPL 3.0',
+          'Homepage: https://ruleset.skk.moe',
+          'GitHub: https://github.com/SukkaW/Surge',
+          '',
+          'This file contains known addresses that are avaliable in the Mainland China.'
+        ],
+        new Date(),
+        results
+      ),
+      path.resolve(__dirname, '../List/non_ip/domestic.conf')
+    ),
+    compareAndWriteFile(
+      [
+        '#!name=[Sukka] Local DNS Mapping',
+        `#!desc=Last Updated: ${new Date().toISOString()}`,
+        '',
+        '[Host]',
+        ...Object.entries(DOMESTICS)
+          .flatMap(
+            ([, { domains, dns }]) => domains.map((domain) => `${domain} = server:${dns}`)
+          ),
+        ''
+      ],
+      path.resolve(__dirname, '../Modules/sukka_local_dns_mapping.sgmodule')
+    )
+  ]);
+})();
---
+++ Source/non_ip/domestic.js
@@ -0,0 +1,127 @@
+// @ts-check
+module.exports.DOMESTICS = /** @type {const} */({
+  ALIBABA: {
+    dns: 'https://223.5.5.5/dns-query',
+    domains: [
+      'uc.cn',
+      'alibaba.com',
+      'alicdn.com',
+      'aliyun.com',
+      'aliyuncs.com',
+      'alikunlun.com',
+      'alipay.com',
+      'cainiao.com',
+      'etao.com',
+      '1688.com',
+      'amap.com',
+      'autonavi.com',
+      'dingtalk.com',
+      'dingtalk.com',
+      'mxhichina.com',
+      'soku.com',
+      'taobao.com',
+      'tmall.com',
+      'tmall.hk',
+      'xiami.com',
+      'xiami.net',
+      'ykimg.com',
+      'youku.com',
+      'ele.me',
+      'elemecdn.com',
+      'taopiaopiao.com',
+      'fliggy.com',
+      'koubei.com',
+      'guoguo-app.com',
+      'mybank.cn'
+    ]
+  },
+  TENCENT: {
+    dns: 'https://120.53.53.53/dns-query',
+    domains: [
+      'dns.pub',
+      'doh.pub',
+      'qcloud.com',
+      'gtimg.com',
+      'idqqimg.com',
+      'igamecj.com',
+      'myapp.com',
+      'myqcloud.com',
+      'qpic.cn',
+      'qlogo.cn',
+      'qq.com',
+      'qq.com.cn',
+      'tencent-cloud.net',
+      'tencent.com',
+      'weixinbridge.com',
+      'weixin.com'
+    ]
+  },
+  BILIBILI: {
+    dns: 'https://120.53.53.53/dns-query',
+    domains: [
+      'acg.tv',
+      'b23.tv',
+      'acgvideo.com',
+      'bilibili.cn',
+      'bilibili.com',
+      'bilibili.tv',
+      'bilivideo.com',
+      'bilivideo.cn',
+      'hdslb.com'
+    ]
+  },
+  XIAOMI: {
+    dns: 'https://120.53.53.53/dns-query',
+    domains: [
+      'mi.com',
+      'duokan.com',
+      'mi-img.com',
+      'mifile.cn',
+      'miui.com',
+      'miwifi.com',
+      'xiaomi.com'
+    ]
+  },
+  BYTEDANCE: {
+    // Currently, bytedance all NS are pointed to AliDNS
+    dns: '180.184.2.2',
+    domains: [
+      'bytecdn.cn',
+      // 'bytescm.com',
+      // 'bytetcc.com',
+      // 'byteimg.com',
+      // 'bytednsdoc.com',
+      'toutiaoimg.com',
+      'feiliao.com',
+      'iesdouyin.com',
+      'pstatp.com',
+      'snssdk.com',
+      'toutiao.com',
+      'feishu.cn',
+      'douyin.com',
+      'douyinpic.com',
+      'douyinstatic.com'
+    ]
+  },
+  BAIDU: {
+    dns: '180.76.76.76',
+    domains: [
+      'baidu.com',
+      'iqiyi.com',
+      'bdstatic.com',
+      'baidubcr.com',
+      'yunjiasu-cdn.net'
+    ]
+  },
+  SYSTEM: {
+    dns: 'system',
+    domains: [
+      'routerlogin.net',
+      '_hotspot_.m2m',
+      'router.asus.com',
+      'hotspot.cslwifi.com',
+      'amplifi.lan',
+      '*.lan'
+    ]
+  }
+});
