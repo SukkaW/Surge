7da10a5cb465c9b4104aaca4bc6055092b52d728
SukkaW <isukkaw@gmail.com>
2024-02-25 19:46:59 +0800

    Update Always RealIP / Steam Hosts

--- Build/build-sgmodule-always-realip.ts
+++ Build/build-sgmodule-always-realip.ts
@@ -9,20 +9,27 @@ const HOSTNAMES = [
   '*.localhost',
   '*.home.arpa',
   // Network Detection, Captive Portal
+  'msftncsi.com',
+  'msftconnecttest.com',
   '*.msftncsi.com',
   '*.msftconnecttest.com',
   'network-test.debian.org',
   'detectportal.firefox.com',
   'resolver1.opendns.com',
+  '*.ipv6.microsoft.com',
   // Handle SNAT conversation properly
   '*.srv.nintendo.net',
   '*.stun.playstation.net',
   'xbox.*.microsoft.com',
   '*.xboxlive.com',
+  'turn.twilio.com',
   '*.turn.twilio.com',
+  'stun.twilio.com',
   '*.stun.twilio.com',
+  'stun.syncthing.net',
   'stun.*.*',
   'stun.*.*.*',
+  'controlplane.tailscale.com',
   // NTP
   'time.*.com', 'time.*.gov, time.*.edu.cn, time.*.apple.com', 'time1.*.com', 'time2.*.com', 'time3.*.com', 'time4.*.com', 'time5.*.com', 'time6.*.com', 'time7.*.com', 'time8.*.com', 'time9.*.com, ntp.*.com, ntp1.*.com, ntp2.*.com, ntp3.*.com, ntp4.*.com, ntp5.*.com, ntp6.*.com, ntp7.*.com', 'time1.*.com', 'time2.*.com', 'time3.*.com', 'time4.*.com', 'time5.*.com', 'time6.*.com', 'time7.*.com', 'time8.*.com', 'ti me9.*.com', '*.time.edu.cn', '*.ntp.org.cn', '*.pool.ntp.org', 'time1.cloud.tencent.com',
   // AdGuard
@@ -35,7 +42,11 @@ const HOSTNAMES = [
   // Microsoft Auto Discovery
   'PDC._msDCS.*.*',
   'DC._msDCS.*.*',
-  'GC._msDCS.*.*'
+  'GC._msDCS.*.*',
+  // Misc,
+  '*.battlenet.com.cn',
+  '*.blzstatic.cn',
+  '*.battlenet.com'
 ] as const;
 
 export const buildAlwaysRealIPModule = task(import.meta.path, async (span) => {
