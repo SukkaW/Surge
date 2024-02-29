0b79bfcd89a560e315a3724fb77f01f921934025
SukkaW <isukkaw@gmail.com>
2024-02-29 14:53:32 +0800

    Update CloudMounter Rule for OneDrive Personal

--- Source/non_ip/cloudmounter.ts
+++ Source/non_ip/cloudmounter.ts
@@ -1,7 +1,10 @@
 export const DOMAINS = [
   'DOMAIN-SUFFIX,sharepoint.com',
   'DOMAIN-SUFFIX,graph.microsoft.com',
-  'DOMAIN,www.googleapis.com'
+  'DOMAIN,www.googleapis.com',
+  'DOMAIN,api.onedrive.com',
+  'DOMAIN-SUFFIX,storage.live.com',
+  'DOMAIN-SUFFIX,files.1drv.com'
 ];
 
 export const PROCESS_NAMES = [
