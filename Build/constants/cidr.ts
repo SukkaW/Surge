2e42705096624cb37d23789fa440b828073a2774
SukkaW <isukkaw@gmail.com>
2024-04-23 21:09:35 +0800

    Update CDN Hosts & Azure China CIDR

--- Build/constants/cidr.ts
+++ Build/constants/cidr.ts
@@ -27,7 +27,15 @@ export const NON_CN_CIDR_INCLUDED_IN_CHNROUTE = [
 // https://github.com/misakaio/chnroutes2/issues/46
 export const CN_CIDR_NOT_INCLUDED_IN_CHNROUTE = [
   '211.99.96.0/19', // wy.com.cn
+
+  '40.72.0.0/15', // AS58593, Azure China, Shanghai
+  '42.159.0.0/16', // AS58593, Azure China, Shanghai
+  '52.130.0.0/17', // AS58593, Azure China, Shanghai
+  '52.131.0.0/16', // AS58593, Azure China, Beijing
+  '103.9.8.0/22', // AS58593, Azure China, Backbone
   '139.217.0.0/16', // AS58593, Azure China, Shanghai
   '139.219.0.0/16', // AS58593, Azure China, Shanghai
-  '143.64.0.0/16' // AS58593, Azure China, Beijing
+  '143.64.0.0/16', // AS58593, Azure China, Beijing
+  '159.27.0.0/16', // AS58593, Azure China, Beijing
+  '163.228.0.0/16' // AS58593, Azure China, Beijing
 ];
