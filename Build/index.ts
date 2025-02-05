4d312c42ff714dcc6022748bb319713d344497ce
SukkaW <isukkaw@gmail.com>
2025-02-05 17:04:23 +0800

    Housekeeping

--- Build/index.ts
+++ Build/index.ts
@@ -69,6 +69,8 @@ const buildFinishedLock = path.join(ROOT_DIR, '.BUILD_FINISHED');
   }
 
   try {
+    await import('why-is-node-running');
+
     const downloadPreviousBuildPromise = downloadPreviousBuild(rootSpan);
     const buildCommonPromise = downloadPreviousBuildPromise.then(() => buildCommon(rootSpan));
 
