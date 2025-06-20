5b09d2ccf451eab0e5108d8e320adabae2cfa0ba
SukkaW <isukkaw@gmail.com>
2025-06-20 17:40:19 +0800

    Update Dist Repo README [skip ci]

--- Build/build-public.ts
+++ Build/build-public.ts
@@ -89,7 +89,9 @@ export const buildPublic = task(require.main === module, __filename)(async (span
     compareAndWriteFile(
       span,
       [
+        '# This is a Robot-managed repo containing only output',
         '# The source code is located at [Sukkaw/Surge](https://github.com/Sukkaw/Surge)',
+        '# Please follow the development at the source code repo instead',
         '',
         '![GitHub repo size](https://img.shields.io/github/repo-size/sukkalab/ruleset.skk.moe?style=flat-square)'
       ],
