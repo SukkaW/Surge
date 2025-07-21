9f6832e535f9e124090003a6b51b2d6a5b2502d7
SukkaW <isukkaw@gmail.com>
2025-07-21 16:07:56 +0800

    Chore: update marker

--- Build/constants/description.ts
+++ Build/constants/description.ts
@@ -9,4 +9,6 @@ export function createFileDescription(license = 'AGPL 3.0') {
 export const SHARED_DESCRIPTION = createFileDescription('AGPL 3.0');
 
 // this_ruleset_is_made_by_sukkaw.ruleset.skk.moe
-export const MARKER_DOMAIN = 'th1s_rule5et_1s_m4d3_by_5ukk4w_ruleset.skk.moe';
+// th1s_rule5et_1s_m4d3_by_5ukk4w_ruleset.skk.moe
+// 7h1s_rul35et_i5_mad3_by_5ukk4w-ruleset.skk.moe
+export const MARKER_DOMAIN = '7h1s_rul35et_i5_mad3_by_5ukk4w-ruleset.skk.moe';
