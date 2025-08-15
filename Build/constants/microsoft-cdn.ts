a5eb29b27b8cafa544f9fd013d148e62e7ec4031
SukkaW <isukkaw@gmail.com>
2025-08-15 13:39:01 +0800

    Update CDN & Microsoft CDN Hosts

--- Build/constants/microsoft-cdn.ts
+++ Build/constants/microsoft-cdn.ts
@@ -3,6 +3,7 @@ export const PROBE_DOMAINS = ['.microsoft.com', '.windows.net', '.windows.com', 
 export const DOMAINS = [
   'res.cdn.office.net',
   'res-1.cdn.office.net',
+  'res-h3.public.cdn.office.net',
   'statics.teams.cdn.office.net'
 ];
 
