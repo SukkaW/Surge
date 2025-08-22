238eb7fa6f6c487e9dc89445d1268ddd520ad4e8
SukkaW <isukkaw@gmail.com>
2025-08-23 01:06:37 +0800

    Chore: update domain alive DoH list

--- Build/lib/is-domain-alive.ts
+++ Build/lib/is-domain-alive.ts
@@ -18,7 +18,7 @@ const dnsServers = [
   'dns10.quad9.net', // Quad9 unfiltered
   'doh.sandbox.opendns.com', // OpenDNS sandbox (unfiltered)
   'unfiltered.adguard-dns.com',
-  // '0ms.dev', // Proxy Cloudflare
+  'v.recipes', // Proxy Cloudflare
   // '76.76.2.0', // ControlD unfiltered, path not /dns-query
   // '76.76.10.0', // ControlD unfiltered, path not /dns-query
   // 'dns.bebasid.com', // BebasID, path not /dns-query but /unfiltered
@@ -28,14 +28,19 @@ const dnsServers = [
   'dns.nextdns.io',
   'anycast.dns.nextdns.io',
   'wikimedia-dns.org',
-  'puredns.org',
   // 'ordns.he.net',
-  // 'dns.mullvad.net',
-  'basic.rethinkdns.com'
+  'dns.mullvad.net',
+  'basic.rethinkdns.com',
+  'doh.qis.io',
+  'dns.surfsharkdns.com',
+  'private.canadianshield.cira.ca',
+  'unfiltered.joindns4.eu',
+  'public.dns.iij.jp',
+  'doh.libredns.gr'
   // '198.54.117.10' // NameCheap DNS, supports DoT, DoH, UDP53
   // 'ada.openbld.net',
   // 'dns.rabbitdns.org'
-].map(dns => 'https://' + dns);
+].map(dns => 'https://' + dns + '/dns-query');
 
 console.log({ dnsServers });
 
