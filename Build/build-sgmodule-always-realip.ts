0c02e84a68bc5833f6f746b51bdb1a52b52dc379
SukkaW <isukkaw@gmail.com>
2026-03-09 22:58:14 +0800

    Add Steam LAN Cache to Real IP

--- Build/build-sgmodule-always-realip.ts
+++ Build/build-sgmodule-always-realip.ts
@@ -27,6 +27,15 @@ const HOSTNAMES = [
   '*.stun.twilio.com',
   'stun.syncthing.net',
   'stun.*',
+  // Steam LAN Cache
+  //
+  // Steam will DNS lookup this domain, trying to find Local LAN Cache server
+  // If one is found, Steam client will try to connect with this IP with original CDN domain in
+  // HTTP Host header, while in HTTP plain HTTP/1.1. It is up to the HTTP server to handle this.
+  //
+  // By having lancache.steamcontent.com in Real IP, we can avoid Steam client accidentally mistaking
+  // the Fake IP as a local LAN cache. This also helps real LAN cache to work properly.
+  'lancache.steamcontent.com',
   // 'controlplane.tailscale.com',
   // NTP
   // 'time.*.com', 'time.*.gov', 'time.*.edu.cn', 'time.*.apple.com', 'time?.*.com', 'ntp.*.com', 'ntp?.*.com', '*.time.edu.cn', '*.ntp.org.cn', '*.pool.ntp.org'
