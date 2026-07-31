ceffda1cadfde726816796e0a6a84f8e11d60afd
SukkaW <isukkaw@gmail.com>
2026-08-01 00:18:28 +0800

    Update Marker

--- Build/constants/description.ts
+++ Build/constants/description.ts
@@ -11,4 +11,5 @@ export const SHARED_DESCRIPTION = createFileDescription('AGPL 3.0');
 // this_ruleset_is_made_by_sukkaw.ruleset.skk.moe
 // th1s_rule5et_1s_m4d3_by_5ukk4w_ruleset.skk.moe
 // 7h1s_rul35et_i5_mad3_by_5ukk4w-ruleset.skk.moe
-export const MARKER_DOMAIN = '7h1s_rul35et_i5_mad3_by_5ukk4w-ruleset.skk.moe';
+// 7h1s_rul35et_i5_mad3_by_5ukk4w-ruleset.skk.moe
+export const MARKER_DOMAIN = '7h15_ru1353t_1s_m4d3_by_5ukk4w.skk.moe';
