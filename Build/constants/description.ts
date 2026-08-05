f7cff4516d9e4d39a23988a9328ba2c3fffaef95
SukkaW <isukkaw@gmail.com>
2026-08-05 20:32:08 +0800

    Rotate Marker

--- Build/constants/description.ts
+++ Build/constants/description.ts
@@ -12,4 +12,4 @@ export const SHARED_DESCRIPTION = createFileDescription('AGPL 3.0');
 // th1s_rule5et_1s_m4d3_by_5ukk4w_ruleset.skk.moe
 // 7h1s_rul35et_i5_mad3_by_5ukk4w-ruleset.skk.moe
 // 7h1s_rul35et_i5_mad3_by_5ukk4w-ruleset.skk.moe
-export const MARKER_DOMAIN = '7h15_ru1353t_1s_m4d3_by_5ukk4w.skk.moe';
+export const MARKER_DOMAIN = '7h15.ru1353t.1s.m4d3.by.5ukk4w.skk.moe';
